9723b6501fd6596a4149acd4269d652a13f0b19b
Anthony Fabian Leon Lucero <anthonyfabian22ll@gmail.com>
2022-05-17 01:02:45 +0200

    Implement annotationProcessor mapstruct

--- src/main/java/com/anthonylldev/store/application/mapper/ItemMapper.java
+++ src/main/java/com/anthonylldev/store/application/mapper/ItemMapper.java
@@ -5,7 +5,7 @@ import com.anthonylldev.store.domain.entity.Item;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = { CategoryMapper.class })
 public interface ItemMapper extends EntityMapper<ItemDto, Item> {
 
 
